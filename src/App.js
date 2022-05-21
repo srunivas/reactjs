117285302803dc4f109973da3fa7169e7fc01665
srunivas <60532589+srunivas@users.noreply.github.com>
2022-05-22 00:12:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+var list  = ["srinivas","syabKhan","Prithvi","Kamesh","srikanth","Gopi","sridhar","mahesh","sudharshan","yash"];
 function App() {
   return (
   <>
       <h1>Srinivas ❤️ 💸💰🪙💲 lot</h1>
+     <section className="Items">
+        {
+          list.map((e) => <div className="Card" style={{color:"white",}}>
+                  <img src={logo} alt="React logo"></img>
+                  <h1>{e}</h1>
+          </div>)
+        }
+      </section>
     </>
   );
 }
